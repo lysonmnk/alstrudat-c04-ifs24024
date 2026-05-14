134fd3374aa2afc8787501e1ec2f33b59884054a
lysonmnk <lysonmanik30@gmail.com>
2026-05-14 16:12:33 +0700

    gas

---
+++ src/main/java/del/alstrudat/App.java
@@ -0,0 +1,11 @@
+package del.alstrudat;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Program.solve(sc);
+        sc.close();
+    }
+}
